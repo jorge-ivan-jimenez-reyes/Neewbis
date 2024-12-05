a0df55f621a60886622ffa25a4c4ca736edfdef4
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2024-12-05 03:56:14 -0600

    tok

--- BACK/NEEBIEBACK/NEEBIEBACK/settings.py
+++ BACK/NEEBIEBACK/NEEBIEBACK/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "news",
     'rest_framework',
     'django_extensions',
+    'rest_framework.authtoken',
 
 ]
 
--- BACK/NEEBIEBACK/news/urls.py
+++ BACK/NEEBIEBACK/news/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from .views import FetchAndClassifyNewsAPIView, NewsListAPIView, UserInteractionAPIView, RecommendationContentBasedAPIView
-
+from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('fetch-classify-news/', FetchAndClassifyNewsAPIView.as_view(), name='fetch-classify-news'),
     path('news/', NewsListAPIView.as_view(), name='news-list'),
     path('user-interaction/', UserInteractionAPIView.as_view(), name='user-interaction'),  # Nuevo endpoint
     path('recommendations/content-based/', RecommendationContentBasedAPIView.as_view(),
          name='recommendations-content-based'),
-
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
