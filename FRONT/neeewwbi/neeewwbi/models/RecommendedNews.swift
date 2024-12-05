54bfa0c03835be6ec88f0e77d5cddb56ef3263b7
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2024-12-05 03:36:47 -0600

    added recommended

---
+++ FRONT/neeewwbi/neeewwbi/models/RecommendedNews.swift
@@ -0,0 +1,14 @@
+//
+//  RecommendedNews.swift
+//  neeewwbi
+//
+//  Created by Jorge Ivan Jimenez Reyes  on 05/12/24.
+//
+
+import Foundation
+
+struct RecommendedNews: Decodable {
+    let id: Int
+    let title: String
+    let summary: String
+}
